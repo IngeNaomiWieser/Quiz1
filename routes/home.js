f753e0b68365c4eee86e83171296dd674c711acb
Inge Wieser <ingewieser@gmail.com>
2017-05-15 19:43:54 -0700

    Final Commit

--- routes/home.js
+++ routes/home.js
@@ -5,8 +5,25 @@ router.get('/', (req, res) => {
   res.render('index')
 });
 
+// Getting the cookies,
+router.post('/dashboard', (req, res) => {
+const name = req.body.name;
+let names = req.cookies.names || [];
+const message = req.body.message;
+let messages = req.cookies.messages || [];
+names.push(name);
+messages.push(message);
+res.cookie('names', names)
+res.cookie('messages', messages)
+res.redirect('/dashboard');
+});
+
+
+// Display the cookies on the screen
 router.get('/dashboard', (req, res) => {
-  res.render('dashboard')
+  let names = req.cookies.names || [];
+  let messages = req.cookies.messages || [];
+  res.render('dashboard', {names: names, messages: messages});
 });
 
 module.exports = router
